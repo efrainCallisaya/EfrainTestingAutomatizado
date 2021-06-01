91097976857b8cc5085cd4ea467d5eb3a91a5351
efrainCallisaya <75282500+efrainCallisaya@users.noreply.github.com>
2021-06-01 20:47:24 -0300

    se agreaga los primeros test con selenium

---
+++ src/test/java/test1/cicoadminweblogin.java
@@ -0,0 +1,45 @@
+package test1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class cicoadminweblogin{
+
+
+    WebDriver driver ;
+    WebDriverWait wait;
+
+    @BeforeMethod
+    public void setUp () {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void login() throws InterruptedException {
+        String siteURL = "https://knowsys.atlassian.net/browse/AA-5";
+        driver.get(siteURL);
+        driver.findElement(By.id("username")).sendKeys("ecallisaya@goiar.com");
+        driver.findElement(By.id("login-submit")).click();
+        Thread.sleep(Long.parseLong("3000"));
+        driver.findElement(By.id("password")).sendKeys("efrain2015");
+        driver.findElement(By.id("login-submit")).click();
+        Thread.sleep(Long.parseLong("15000"));
+        driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/span[2]/div/div/button[1]")).click();
+        List<WebElement> elements = driver.findElements(By.tagName("h1"));
+
+        ////*[@id="tempoIssueViewPanel"]/div[1]/span[2]/div/div
+       // river.findElement(By.xpath("//*[@id='tempoIssueViewPanel']/div[1]/span[2]/div/div")).click();
+        //driver.findElement(By.xpath("/html/body/div[2]/div/div/div[1]/span[2]/div/div/button[1]"));
+
+    }
+}
---
+++ src/test/java/testcicopersonal/test1.java
@@ -0,0 +1,79 @@
+package testcicopersonal;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class test1 {
+
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    @BeforeMethod
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 15);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        String siteURL = "https://testing.cicoadmin.goiar.com/#/admin/dashboard";
+        driver.get(siteURL);
+        Assert.assertEquals("https://testing.cicoadmin.goiar.com/#/login", driver.getCurrentUrl());
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[@placeholder='Usuario']")).sendKeys("admin@cico.com");
+        driver.findElement(By.xpath("//*[@placeholder='Contraseña']")).sendKeys("cicO2020");
+        System.out.println("la URL es: " + driver.getCurrentUrl());
+        driver.findElement(By.xpath("//*[@id=\"root\"]/section/main/div/div/form/div[3]/div")).click();
+
+        Thread.sleep(3000);
+        //Buscamos el elemento de  tags modificar lo que hay en las "" para buscar otro tags
+
+        List<WebElement> searchelemts = driver.findElements(By.tagName("span"));
+        Assert.assertNotNull(searchelemts);
+        Assert.assertNotEquals(searchelemts.size(), 0);
+
+        boolean elementovalido = false;
+        for(WebElement h2: searchelemts){
+            if (h2.getText().equals("Actividad reciente")){
+                elementovalido = true;
+            }
+        }
+        Assert.assertTrue(elementovalido);
+
+        driver.navigate().back();
+        /*WebElement getBtnByText = getBtnByText("Inicia Sesion");
+        getBtnByText.click();
+
+
+        private WebElement getBtnByText (String getBtnByText){
+            List<WebElement> btnList = driver.findElements(By.className("typography"));
+            Assert.assertNotEquals(btnList.size(), 0);
+
+            for (WebElement btn : btnList) {
+                if (btn.getText().equals(getBtnByText)) {
+                    return btn;
+                }
+            }
+
+            Assert.assertFalse(true, "No se encontro el elemento");
+            return null;
+
+        }*/
+    }
+}
+
+
+
